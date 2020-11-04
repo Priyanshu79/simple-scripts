a864857d2e56b9735ea87f7be2ff9d85004b7387
Priyanshu Mishra <67860164+Priyanshu79@users.noreply.github.com>
2020-11-05 04:17:29 +0530

    Create area_of_a_rectangle.c

---
+++ area_of_a_rectangle.c
@@ -0,0 +1,17 @@
+//a program to find area of a rctangle with integer sized sides? lol
+
+#include<stdio.h>
+
+int main(){
+
+    int length, breadth;
+    printf("Enter the length\n");
+    scanf("%d", &length);
+
+    printf("Enter the breadth\n");
+    scanf("%d", &breadth);
+
+    printf("The area of your rectangle is %d", length*breadth);
+    
+    return 0;
+}
